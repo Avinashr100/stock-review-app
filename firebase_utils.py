67f30169a570934e6676f329d3f79fda75f2702e
Avinashr100 <avinash.official1@gmail.com>
2025-06-01 20:11:53 +0530

    firebase_utils.py

--- firebase_utils.py
+++ firebase_utils.py
@@ -4,7 +4,22 @@ import os
 
 def init_firebase():
     if not firebase_admin._apps:
-        cred = credentials.Certificate("firebase_key.json")
+from firebase_admin import credentials
+from streamlit.runtime.secrets import secrets
+
+cred = credentials.Certificate({
+    "type": secrets.firebase.type,
+    "project_id": secrets.firebase.project_id,
+    "private_key_id": secrets.firebase.private_key_id,
+    "private_key": secrets.firebase.private_key,
+    "client_email": secrets.firebase.client_email,
+    "client_id": secrets.firebase.client_id,
+    "auth_uri": secrets.firebase.auth_uri,
+    "token_uri": secrets.firebase.token_uri,
+    "auth_provider_x509_cert_url": secrets.firebase.auth_provider_x509_cert_url,
+    "client_x509_cert_url": secrets.firebase.client_x509_cert_url,
+})
+
         firebase_admin.initialize_app(cred, {
             "databaseURL": "https://stock-review-c05a5-default-rtdb.firebaseio.com/"
         })
