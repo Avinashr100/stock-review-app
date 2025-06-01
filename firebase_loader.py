df26bdbe94d332b15ba813b58e3b6c382eb8c1dc
Avinashr100 <avinash.official1@gmail.com>
2025-06-01 20:17:40 +0530

    Add files via upload

--- firebase_loader.py
+++ firebase_loader.py
@@ -1,22 +1,21 @@
 import streamlit as st
-from firebase_admin import credentials, initialize_app
-from streamlit.runtime.secrets import secrets
 import firebase_admin
+from firebase_admin import credentials
 
 def init_firebase():
     if not firebase_admin._apps:
         cred = credentials.Certificate({
-            "type": secrets.firebase.type,
-            "project_id": secrets.firebase.project_id,
-            "private_key_id": secrets.firebase.private_key_id,
-            "private_key": secrets.firebase.private_key,
-            "client_email": secrets.firebase.client_email,
-            "client_id": secrets.firebase.client_id,
-            "auth_uri": secrets.firebase.auth_uri,
-            "token_uri": secrets.firebase.token_uri,
-            "auth_provider_x509_cert_url": secrets.firebase.auth_provider_x509_cert_url,
-            "client_x509_cert_url": secrets.firebase.client_x509_cert_url
+            "type": st.secrets["firebase"]["type"],
+            "project_id": st.secrets["firebase"]["project_id"],
+            "private_key_id": st.secrets["firebase"]["private_key_id"],
+            "private_key": st.secrets["firebase"]["private_key"],
+            "client_email": st.secrets["firebase"]["client_email"],
+            "client_id": st.secrets["firebase"]["client_id"],
+            "auth_uri": st.secrets["firebase"]["auth_uri"],
+            "token_uri": st.secrets["firebase"]["token_uri"],
+            "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+            "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
         })
-        initialize_app(cred, {
+        firebase_admin.initialize_app(cred, {
             "databaseURL": "https://stock-review-c05a5-default-rtdb.firebaseio.com/"
         })
